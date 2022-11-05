455ad1f6ba3e3a947674076ff47f493c11f153b7
Danexy123 <111960893+Danexy123@users.noreply.github.com>
2022-11-05 18:22:24 +0500

    Add files via upload

---
+++ LAB3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double a = 0.1, b = 1, k = 10, shag, y, n = 10, x, SN = 1, EL1, EL2, SE = 1;
+	const double E = 0.0001;
+
+	x = a;
+
+	shag = (b - a) / k;
+
+	while (x < b)
+	{
+		y = pow(3, x);
+
+		for (int i = 1; i <= n; i++)
+		{
+			SN += pow(log(3), i) * pow(x, i) / i;
+		}
+
+		int h = 1;
+
+		EL1 = 1;
+
+		EL2 = pow(log(3), h) * pow(x, h) / h;
+
+		while (abs(EL2 - EL1) > E)
+		{
+			EL1 = EL2;
+
+			SE += EL1;
+
+			h++;
+
+			EL2 = pow(log(3), h) * pow(x, h) / h;
+
+		}
+		x += shag;
+
+		cout << "X = " << x;
+		cout << "\tSN = " << SN;
+		cout << "\tSE = " << SE;
+		cout << "\tY = " << y << endl;
+	}
+	return 0;
+}
